45e5ee41a75cf9b2d48084688ef892a38ded769b
Maryam Ali <mali62541@gmail.com>
2017-02-16 19:12:52 -0500

    working

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  for ( var i = 0; i < works.length; ++i) {
+    $("#work-row").append("\
+      <div class='col-md-3 col-xs-12'>\
+          <div class='hover-text'>\
+            <a href=" + works[i].URL + " class='work-img'>\
+            <span class='info'><p class='proj-title'>Title:</p> " + works[i].title + " </span>\
+              <img class='img-responsive' src='" + works[i].pic + "'>\
+            </a>\
+          </div>\
+    ");
+
+    var images = $("#work-row img");
+
+    if(i%2 === 0){
+      //make border blue
+      $(images[i]).css("border", "2px solid #0284F0");
+
+    } else {
+      //make border grey
+      $(images[i]).css("border", "2px solid #858585");
+    };
+
+    $(".work-img").mouseenter( function() {
+        $(".info", this).show();
+    }).mouseleave(function (){
+        $(".info", this).hide();
+    });
+
+  };
+
+});
---
+++ js/work.js
@@ -0,0 +1,57 @@
+var works = [
+  {
+    title: "Mapping Data Keys (press the keys)",
+    pic: "./img/Work/dinosaur.svg",
+    URL: "http://dinodata.surge.sh/" 
+
+  },
+
+  {
+    title: "CSS Flexbox (click the panels)",
+    pic: "./img/Work/coffee.svg",
+    URL: "http://flexpanels.surge.sh/" 
+
+  },
+
+  {
+    title: "JS & CSS Clock",
+    pic: "./img/Work/clock.svg",
+    URL: "http://clockjs.surge.sh/" 
+
+  },
+
+  {
+    title: "Fourth Project",
+    pic: "./img/Work/laptop.svg",
+    URL: "https://placekitten.com/" 
+
+  },
+
+  {
+    title: "Fifth Project",
+    pic: "./img/Work/monitor.svg",
+    URL: "https://placekitten.com/" 
+
+  },
+
+  {
+    title: "Sixth Project",
+    pic: "./img/Work/smartwatch.svg",
+    URL: "https://placekitten.com/" 
+
+  },
+
+  {
+    title: "Seventh Project",
+    pic: "./img/Work/tablet.svg",
+    URL: "https://placekitten.com/" 
+
+  },
+
+  {
+    title: "Eight Project",
+    pic: "./img/Work/video-player.svg",
+    URL: "https://placekitten.com/" 
+
+  }
+];
